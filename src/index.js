846a3c611abe6045d3093bfdd92eaac2edb67c84
vijaya2003 <20b01a1236@svecw.edu.in>
2022-02-15 15:18:28 +0530

    third

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+
+function Table(){
+  return(
+    <table className="ui very basic collapsing celled table">
+  <thead>
+    <tr><th>Employee</th>
+    <th>Correct Guesses</th>
+  </tr></thead>
+  <tbody>
+    <tr>
+      <td>
+        <h4 className="ui image header">
+          <img src={faker.image.image()} className="ui mini rounded image"/>
+          <div className="content">
+            Lena
+            <div className="sub header">Human Resources
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        22
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 className="ui image header">
+          <img src={faker.image.image()}className="ui mini rounded image"/>
+          <div className="content">
+            Matthew
+            <div className="sub header">Fabric Design
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        15
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 className="ui image header">
+          <img src={faker.image.image()} className="ui mini rounded image"/>
+          <div className="content">
+            Lindsay
+            <div className="sub header">Entertainment
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        12
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 className="ui image header">
+          <img src={faker.image.image()} className="ui mini rounded image"/>
+          <div className="content">
+            Mark
+            <div className="sub header">Executive
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        11
+      </td>
+    </tr>
+  </tbody>
+</table>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ 
+  <>
+  <Table/>
+  </>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
